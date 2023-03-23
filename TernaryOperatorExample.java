d66dbd8fcc8b3cf5306ab37d030cf4a7b1c76f32
Krishan <63546119+krishanvarathe@users.noreply.github.com>
2023-03-23 17:27:33 +0000

    TernaryOperatorExample

---
+++ TernaryOperatorExample.java
@@ -0,0 +1,10 @@
+public class TernaryOperatorExample {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 50;
+        int result = (a>b) ? a:b;
+        System.out.println(result);
+
+    }
+    
+}
