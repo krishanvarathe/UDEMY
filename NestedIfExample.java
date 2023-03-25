8854401cffac43a36cdfa95ccd88af06f5f5cf3c
Krishan <63546119+krishanvarathe@users.noreply.github.com>
2023-03-25 17:53:43 +0000

    commit changes

---
+++ NestedIfExample.java
@@ -0,0 +1,12 @@
+public class NestedIfExample {
+    public static void main(String[] args) {
+        int num=50;
+        if(num<100){
+            System.out.println("number is smaller then 100");
+            if(num>25){
+                System.out.println("number is greater then 25");
+            }
+        }
+    }
+    
+}
