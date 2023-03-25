a5d3a8983c0f0dd95358e9f136f05367e8b696b9
Krishan <63546119+krishanvarathe@users.noreply.github.com>
2023-03-25 17:21:16 +0000

    commit

---
+++ IfExample.java
@@ -0,0 +1,8 @@
+public class IfExample{
+    public static void main(String[] args) {
+        int num=50;
+        if(num>20){
+            System.out.println("Number is greater");
+        }
+    }
+}
