f1836b41e56f41640301cefbf338d5ee3280f8f5
Krishan <63546119+krishanvarathe@users.noreply.github.com>
2023-03-26 18:04:28 +0000

    26/03/2023

---
+++ SwitchCaseExample.java
@@ -0,0 +1,25 @@
+public class SwitchCaseExample {
+    public static void main(String[] args) {
+        int i=8;
+        switch(i){
+            case 1:
+            System.out.println("Switch Case 1");
+            break;
+            case 2:
+            System.out.println("Switch Case 2");
+            break;
+            case 3:
+            System.out.println("Switch Case 3");
+            break;
+            case 4:
+            System.out.println("Switch Case 4");
+            break;
+            case 5:
+            System.out.println("Switch Case 5");
+            break;
+            default:
+            System.out.println("Default case");
+        }
+    }
+    
+}
