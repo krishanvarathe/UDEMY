31df363389b8cc7517ae5e506f1049c1ed96b649
Krishan <63546119+krishanvarathe@users.noreply.github.com>
2023-03-26 17:42:23 +0000

    26/03/2023

---
+++ FindBiggestNumber.java
@@ -0,0 +1,17 @@
+public class FindBiggestNumber {
+    public static void main(String[] args) {
+        
+    
+    int a=100, b=200, c=70;
+
+    if(a>=b && a>=c){
+        System.out.println("A is Greather: "+a);
+    }
+    else if(b>=c && b>=a){
+        System.out.println("B is Greather: "+b);
+    }
+    else{
+        System.out.println("C is Greather: "+c);
+    }
+}
+}
